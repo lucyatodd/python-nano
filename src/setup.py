db8934bf9322b55b906150179bf5cd1a67be01d8
Paul Todd <paulxtodd@gmail.com>
2019-02-26 21:10:41 +0000

    Breaking up Jupyter 1: imports

---
+++ src/setup.py
@@ -0,0 +1,7 @@
+# import all packages and set plots to be embedded inline
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+get_ipython().run_line_magic('matplotlib', 'inline')
