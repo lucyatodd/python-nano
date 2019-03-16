4bc0f1120df65d124b54d39836942c0f9fab1f36
Paul Todd <paulxtodd@gmail.com>
2019-03-16 15:58:32 +0000

    CLI improved

--- src/cli.py
+++ src/cli.py
@@ -5,20 +5,30 @@ from utilities.text import fnSay
 
 @click.command()
 # @click.option('--count', default=1, help='Number of greetings.')
-@click.option('--function', prompt="function name: sinmatplot|hello", help='A function to call.')
-@click.option('--arg', default="", help='argument')
+@click.option('--function', required=False, help='call a function')
+@click.option('--list', required=False, help='list functions.')
+@click.option('--arg', required=False, default="", help='argument')
 
-def commands(function, arg):
+def commands(function, arg, list):
     """Simple program that greets NAME for a total of COUNT times."""
-    if function:
+    if list:
+       print("functions: sinmatplot | hello")
+    elif function:
        if function == "sinmatplot":
           chart()
        if function == "hello":
           fnSay(arg)
        else:
-          print("Function " + function + " does not exit")
+          raise AssertionError("Function " + function + " does not exit")
     else:
-      print("Please supply a function")
-
+       raise AssertionError("usage: cli.py --function --list --arg")
+ 
 if __name__ == '__main__':
+   try:
     commands()
+   except AssertionError as error:
+    print(error)
+   else: 
+    print("No exceptions!")
+   finally:
+    print("Complete!")
