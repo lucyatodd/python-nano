d006cd77abe47e0ddd62788c9c1e31f00047e308
Lucy Todd <lucytodd@Lucys-MacBook-2.local>
2019-04-07 13:02:25 +0100

    zoo

--- src/mysql/zoo.py
+++ src/mysql/zoo.py
@@ -17,44 +17,35 @@ cnx = connection.MySQLConnection(user=user, password=password, host=host, port=p
 cursor = database.cursor()
 
 # read data
-data = pd.read_excel('Zoo_data.xls')
-
-# export
-data.to_excel('Zoo_data.xls', index=False)
-
-# Open the workbook and define the worksheet
-book = xlrd.open_workbook("Zoo_data.xls")
-sheet = book.sheet_by_name("Sheet1")
+book = pd.read_excel('Zoo_data.xlsx')
+book.head()
 
 query1 = """
-CREATE TABLE [LEAF].[ZZZ] (
-    Name varchar(255),
-    Class varchar(255),
-    Population varchar(255)
-)"""
+    CREATE TABLE [Zoo](
+        Name varchar(255),
+        Class varchar(255),
+        Population varchar(255)
+    )"""
 
 query = """
-INSERT INTO [LEAF].[ZZZ] (
-     Name varchar(255),
-    Class varchar(255),
-    Population varchar(255)
-) VALUES (?, ?, ?)"""
+    INSERT INTO [Zoo] (
+        Name varchar(255),
+        Class varchar(255),
+        Population varchar(255)
+    ) VALUES (?, ?, ?)"""
 
 # execute create table
-try:
-    cursor.execute(query1)
-    conn.commit()
-except pyodbc.ProgrammingError:
-    pass
+cursor.execute(query1)
+cnx.commit()
 
 # grab existing row count in the database for validation later
-cursor.execute("SELECT count(*) FROM LEAF.ZZZ")
+cursor.execute("SELECT count(*) FROM Zoo")
 before_import = cursor.fetchone()
 
-for r in range(1, sheet.nrows):
-    Name = sheet.cell(r,0).value
-    Class = sheet.cell(r,1).value
-    Population = sheet.cell(r,2).value
+for r in range(1, book.nrows):
+    Name = book.cell(r,0).value
+    Class = book.cell(r,1).value
+    Population = book.cell(r,2).value
 
     # Assign values from each row
     values = (Name, Class, Population)
@@ -66,10 +57,10 @@ for r in range(1, sheet.nrows):
 cnx.commit()
 
 # If you want to check if all rows are imported
-cursor.execute("SELECT count(*) FROM LEAF.ZZZ")
+cursor.execute("SELECT count(*) FROM Zoo")
 result = cursor.fetchone()
 
-print((result[0] - before_import[0]) == len(data.index))  # should be True
+print((result[0] - before_import[0]) == len(book.index))  # should be True
 
 # Close the database connection
 cnx.close()
